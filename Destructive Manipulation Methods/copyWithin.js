548eac033e875d638a1ef5adffead95c7c7bb8c3
Keane Small <48247528+Keane-Small@users.noreply.github.com>
2023-11-30 13:19:41 +0200

    Create copyWithin.js

---
+++ Destructive Manipulation Methods/copyWithin.js
@@ -0,0 +1,13 @@
+// The copyWithin() method copies array elements from one position to another in the given array.
+
+let numbers = [1, 2, 3, 4, 5];
+
+numbers.copyWithin(0, 4);
+
+console.log(numbers);
+
+// let letters = ["a", "b", "c", "d"];
+
+// letters.copyWithin(2, 1);
+
+// console.log(letters);
